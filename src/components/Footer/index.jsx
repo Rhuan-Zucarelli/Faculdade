64cfcb989bfbd6adcc3c7b0f1d112a537e484393
Rhuan-Zucarelli <rhuan1zucarelli@gamil.com>
2022-09-12 19:20:09 -0300

    Alteração no cod.

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <footer
+            style={{
+                background: '#0d6efd',
+                paddingTop: 15,
+                paddingBottom: 15,
+                marginTop: 15,
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}
+        >
+            <p>Desenvolvido por seu nome :D</p>
+        </footer>
+    )
+}
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import './styles.css'
+
+const Header = () => {
+    return(
+        <header
+            style={{
+                background: '#1d1d1d'
+            }}
+        >
+            <div className="divHeader">
+                <h1>Cinema em casa</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
